089840ec19dc254f77504f41fb3a0775d3f26f29
BhoomiShakya-Meridian <bhoomi.shakya@onmeridian.com>
2025-02-21 13:25:46 +0530

    Fix 404 issue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",  // Ensures correct asset loading
+  build: {
+    outDir: "dist",  // Must match GitHub Actions `output_location`
+  },
   plugins: [react(),tailwindcss()],
 })
